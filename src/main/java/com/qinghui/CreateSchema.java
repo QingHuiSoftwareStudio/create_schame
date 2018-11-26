4642421e68fc739aafdb5d411dab1852915e67a1
QingHuiSoftwareStudio <2710393@qq.com>
2018-11-27 01:36:58 +0800

    修改动态生成fieldType标签。

--- src/main/java/com/qinghui/CreateSchema.java
+++ src/main/java/com/qinghui/CreateSchema.java
@@ -22,6 +22,7 @@ public class CreateSchema {
     private static final String EXCEL_PATH = "G:\\develope\\schema.xlsx";
     private static final String FILE_TYPE_1 = "xls";
     private static final String FILE_TYPE_2 = "xlsx";
+    private static String WOOK_BOOK_TYPE = null;
 
     public static void main(String[] args) throws Exception {
 
@@ -33,9 +34,11 @@ public class CreateSchema {
             String[] split = excel.getName().split("\\.");
             Workbook wb = null;
             if(FILE_TYPE_1.equals(split[1])) {
+                WOOK_BOOK_TYPE = FILE_TYPE_1;
                 FileInputStream inputStream = new FileInputStream(excel);
                 wb = new HSSFWorkbook(inputStream);
             }else if(FILE_TYPE_2.equals(split[1])) {
+                WOOK_BOOK_TYPE = FILE_TYPE_2;
                 wb = new XSSFWorkbook(excel);
             }else {
                 throw new RuntimeException("只支持xls和xlsx格式的文件!");
@@ -79,14 +82,10 @@ public class CreateSchema {
         bw.newLine();
 
         // 生成fieldType字段
-        // ik
-        createIKFieldType(sheet, bw);
+        createIKFieldType2(sheet, bw);
         // 生成其它常用类型字段
         createOtherFieldType(bw);
-        bw.newLine();
 
-        bw.write("</schema>");
-        bw.close();
     }
 
     /**
@@ -95,73 +94,106 @@ public class CreateSchema {
      * @throws IOException
      */
     private static void createOtherFieldType(BufferedWriter bw) throws IOException {
-        bw.write("\t<fieldType name=\"string\" class=\"solr.StrField\" sortMissingLast=\"true\" />");
-        bw.newLine();
-        bw.write("\t<fieldType name=\"boolean\" class=\"solr.BoolField\" sortMissingLast=\"true\"/>");
-        bw.newLine();
-        bw.write("\t<fieldType name=\"int\" class=\"solr.TrieIntField\" precisionStep=\"0\" positionIncrementGap=\"0\"/>");
-        bw.newLine();
-        bw.write("\t<fieldType name=\"float\" class=\"solr.TrieFloatField\" precisionStep=\"0\" positionIncrementGap=\"0\"/>");
-        bw.newLine();
-        bw.write("\t<fieldType name=\"long\" class=\"solr.TrieLongField\" precisionStep=\"0\" positionIncrementGap=\"0\"/>");
-        bw.newLine();
-        bw.write("\t<fieldType name=\"double\" class=\"solr.TrieDoubleField\" precisionStep=\"0\" positionIncrementGap=\"0\"/>");
-        bw.newLine();
-        bw.write("\t<fieldType name=\"date\" class=\"solr.TrieDateField\" precisionStep=\"0\" positionIncrementGap=\"0\"/>");
-        bw.newLine();
-        bw.write("\t<fieldType name=\"text_general\" class=\"solr.TextField\" positionIncrementGap=\"100\">");
-        bw.newLine();
-        bw.write("\t\t<analyzer type=\"index\">");
-        bw.newLine();
-        bw.write("\t\t\t<tokenizer class=\"solr.StandardTokenizerFactory\"/>");
-        bw.newLine();
-        bw.write("\t\t\t<filter class=\"solr.StopFilterFactory\" ignoreCase=\"true\" words=\"stopwords.txt\" />");
-        bw.newLine();
-        bw.write("\t\t\t<filter class=\"solr.LowerCaseFilterFactory\"/>");
-        bw.newLine();
-        bw.write("\t\t</analyzer>");
-        bw.newLine();
-        bw.write("\t\t<analyzer type=\"query\">");
-        bw.newLine();
-        bw.write("\t\t\t<tokenizer class=\"solr.StandardTokenizerFactory\"/>");
-        bw.newLine();
-        bw.write("\t\t\t<filter class=\"solr.StopFilterFactory\" ignoreCase=\"true\" words=\"stopwords.txt\" />");
-        bw.newLine();
-        bw.write("\t\t\t<filter class=\"solr.SynonymFilterFactory\" synonyms=\"synonyms.txt\" ignoreCase=\"true\" expand=\"true\"/>");
-        bw.newLine();
-        bw.write("\t\t\t<filter class=\"solr.LowerCaseFilterFactory\"/>");
-        bw.newLine();
-        bw.write("\t\t</analyzer>");
-        bw.newLine();
-        bw.write("\t</fieldType>");
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(CreateSchema.class.getClassLoader().getResourceAsStream("schema.txt"))))
+        {
+            String readLine = br.readLine();
+            while (readLine != null) {
+                bw.write(readLine);
+                bw.newLine();
+                readLine = br.readLine();
+            }
+        }catch (FileNotFoundException e) {
+            throw new RuntimeException("请检查classpath下的schema.txt文件是否存在，且内容未被修改!");
+        }
+        bw.close();
     }
 
     /**
-     * 生成其ik字段
-     * @param sheet 文件配置信息excel(数据源)
+     * 生成三方fieldType字段
+     * @param sheet
      * @param bw
      * @throws IOException
      */
-    private static void createIKFieldType(Sheet sheet, BufferedWriter bw) throws IOException {
-        Row rowFTField = sheet.getRow(7);
-        if(rowFTField != null) {
-            Cell cellFTField = rowFTField.getCell(23);
-            if(!checkCellIsNull(cellFTField)) {
-                StringBuffer sb = new StringBuffer();
-                sb.append("\t<fieldType ");
-                sb = appendStr(sb, "name", cellFTField, 7, 23);
-                sb.append("class=\"solr.TextField\">");
-                bw.write(sb.toString());
-                bw.newLine();
-                bw.write("\t\t<analyzer type=\"index\" class=\"org.wltea.analyzer.lucene.IKAnalyzer\" />");
-                bw.newLine();
-                bw.write("\t\t<analyzer type=\"query\" class=\"org.wltea.analyzer.lucene.IKAnalyzer\" />");
-                bw.newLine();
-                bw.write("\t</fieldType>");
+    private static void createIKFieldType2(Sheet sheet, BufferedWriter bw) throws IOException {
+        StringBuffer sb = new StringBuffer();
+        for (int start = 7;start < sheet.getLastRowNum(); start++) {
+
+            Row rowFTField = sheet.getRow(start);
+            // 判断fieldType字段这一行是否有值
+            if(rowFTField != null) {
+                // 判断fieldType字段是否输入了name值
+                if(checkCellIsNull(rowFTField.getCell(23))) {
+                    // 判断analyzer是否有值
+                    if(checkCellIsNull(rowFTField.getCell(26))) {
+                        // 判断filter是否有值
+                        if(checkCellIsNull(rowFTField.getCell(29))) {
+                            //bw.write("</analyzer>");
+                        }else {
+                            // 拼接filter
+                            sb.append("\t\t\t<filter ");
+                            sb = appendFieldTypeStr(sb,"class", rowFTField, 29);
+                            sb = appendFieldTypeStr(sb,"ignoreCase", rowFTField, 30);
+                            sb = appendFieldTypeStr(sb,"words", rowFTField, 31);
+                            sb = appendFieldTypeStr(sb,"format", rowFTField, 32);
+                            sb.append("/>");
+                            if(sheet.getRow(rowFTField.getRowNum() + 1) == null || sheet.getRow(rowFTField.getRowNum() + 1).getCell(23) != null) {
+                                sb.append("\r\n\t\t</analyzer>");
+                                sb.append("\r\n\t</fieldType>\r\n\r\n");
+                            }
+                        }
+                    }else {
+                        // 拼接analyzer
+                        sb.append("\r\n\t\t</analyzer>\r\n ");
+                        sb.append("\t\t<analyzer ");
+                        sb = appendFieldTypeStr(sb,"type", rowFTField, 26);
+                        sb.append(">\r\n");
+                        // 拼接tokenizer
+                        sb.append("\t\t\t<tokenizer ");
+                        sb = appendFieldTypeStr(sb,"class", rowFTField, 27);
+                        sb = appendFieldTypeStr(sb,"mode", rowFTField, 28);
+                        sb.append("/>\r\n");
+                        // 拼接filter
+                        sb.append("\t\t\t<filter ");
+                        sb = appendFieldTypeStr(sb,"class", rowFTField, 29);
+                        sb = appendFieldTypeStr(sb,"ignoreCase", rowFTField, 30);
+                        sb = appendFieldTypeStr(sb,"words", rowFTField, 31);
+                        sb = appendFieldTypeStr(sb,"format", rowFTField, 32);
+                        sb.append("/>\r\n");
+                    }
+                }else {
+                    if(start != 7){
+                        sb.append("\r\n\t</analyzer>\r\n ");
+                        sb.append("\t</fieldType>\r\n ");
+                    }
+                    sb.append("\t<fieldType ");
+                    sb = appendFieldTypeStr(sb,"name", rowFTField, 23);
+                    sb = appendFieldTypeStr(sb,"class", rowFTField, 24);
+                    sb = appendFieldTypeStr(sb,"positionIncrementGap", rowFTField, 25);
+                    sb.append(">\r\n");
+                    // 拼接analyzer
+                    sb.append("\t\t<analyzer ");
+                    sb = appendFieldTypeStr(sb,"type", rowFTField, 26);
+                    sb.append(">\r\n");
+                    // 拼接tokenizer
+                    sb.append("\t\t\t<tokenizer ");
+                    sb = appendFieldTypeStr(sb,"class", rowFTField, 27);
+                    sb = appendFieldTypeStr(sb,"mode", rowFTField, 28);
+                    sb.append("/>\r\n");
+                    // 拼接filter
+                    sb.append("\t\t\t<filter ");
+                    sb = appendFieldTypeStr(sb,"class", rowFTField, 29);
+                    sb = appendFieldTypeStr(sb,"ignoreCase", rowFTField, 30);
+                    sb = appendFieldTypeStr(sb,"words", rowFTField, 31);
+                    sb = appendFieldTypeStr(sb,"format", rowFTField, 32);
+                    sb.append("/>\r\n");
+                }
             }
-            bw.newLine();
         }
+        bw.write(sb.toString());
+        bw.flush();
     }
+
 
     /**
      * 生成copyField字段
@@ -182,10 +214,10 @@ public class CreateSchema {
 
             StringBuffer sb = new StringBuffer();
             sb.append("\t<copyField ");
-            sb = appendStr(sb, "source", cellCFSource, start, 19);
+            sb = appendStr(sb, "source", cellCFSource, 19);
 
             Cell cellCFDest = sheet.getRow(start).getCell(20);
-            sb = appendStr(sb, "dest", cellCFDest, start, 20);
+            sb = appendStr(sb, "dest", cellCFDest, 20);
 
             sb.append("/>");
             bw.write(sb.toString());
@@ -231,19 +263,19 @@ public class CreateSchema {
 
             StringBuffer sb = new StringBuffer();
             sb.append("\t<dynamicField ");
-            sb = appendStr(sb, "name", cellDynamicField, start, 9);
+            sb = appendStr(sb, "name", cellDynamicField, 9);
 
             Cell cellDYType = sheet.getRow(start).getCell(10);
-            sb = appendStr(sb, "type", cellDYType, start, 10);
+            sb = appendStr(sb, "type", cellDYType, 10);
 
             Cell cellDYIndexed = sheet.getRow(start).getCell(11);
-            sb = appendStr(sb, "indexed", cellDYIndexed, start, 11);
+            sb = appendStr(sb, "indexed", cellDYIndexed, 11);
 
             Cell cellDYStored = sheet.getRow(start).getCell(12);
-            sb = appendStr(sb, "stored", cellDYStored, start, 12);
+            sb = appendStr(sb, "stored", cellDYStored, 12);
 
             Cell cellDYMultiValued = sheet.getRow(start).getCell(13);
-            sb = appendStr(sb, "multiValued", cellDYMultiValued, start, 13);
+            sb = appendStr(sb, "multiValued", cellDYMultiValued, 13);
 
             sb.append("/>");
             bw.write(sb.toString());
@@ -270,22 +302,22 @@ public class CreateSchema {
 
             StringBuffer sb = new StringBuffer();
             sb.append("\t<field ");
-            sb = appendStr(sb, "name", cellField, start, 1);
+            sb = appendStr(sb, "name", cellField, 1);
 
             Cell cellFieldType = sheet.getRow(start).getCell(2);
-            sb = appendStr(sb, "type", cellFieldType, start, 2);
+            sb = appendStr(sb, "type", cellFieldType, 2);
 
             Cell cellFieldIndexed = sheet.getRow(start).getCell(3);
-            sb = appendStr(sb, "indexed", cellFieldIndexed, start, 3);
+            sb = appendStr(sb, "indexed", cellFieldIndexed, 3);
 
             Cell cellFieldStored = sheet.getRow(start).getCell(4);
-            sb = appendStr(sb, "stored", cellFieldStored, start, 4);
+            sb = appendStr(sb, "stored", cellFieldStored, 4);
 
             Cell cellFieldRequired = sheet.getRow(start).getCell(5);
-            sb = appendStr(sb, "required", cellFieldRequired, start, 5);
+            sb = appendStr(sb, "required", cellFieldRequired, 5);
 
             Cell cellMultiValued = sheet.getRow(start).getCell(6);
-            sb = appendStr(sb, "multiValued", cellMultiValued, start, 6);
+            sb = appendStr(sb, "multiValued", cellMultiValued, 6);
 
             sb.append("/>");
             bw.write(sb.toString());
@@ -298,15 +330,32 @@ public class CreateSchema {
      * @param sb
      * @param attrName 属性名称
      * @param cell 属性值对应的单元格对象
-     * @param rowIndex 行索引
      * @param cellIndex 列索引
      * @return
      */
-    private static StringBuffer appendStr(StringBuffer sb, String attrName, Cell cell, int rowIndex, int cellIndex) {
+    private static StringBuffer appendStr(StringBuffer sb, String attrName, Cell cell, int cellIndex) {
         if(checkCellIsNull(cell)) {
             sb.append(attrName).append("=\"").append(sheet.getRow(6).getCell(cellIndex).getStringCellValue()).append("\" ");
         }else {
             sb.append(attrName).append("=\"").append(cell.getStringCellValue()).append("\" ");
+        }
+        return sb;
+    }
+
+    /**
+     * 拼接fieldType属性字符串
+     * @param sb
+     * @param attrName
+     * @param row
+     * @param cellIndex
+     * @return
+     */
+    private static StringBuffer appendFieldTypeStr(StringBuffer sb, String attrName, Row row, int cellIndex) {
+        if(checkCellIsNull(row.getCell(cellIndex))) {
+            row.createCell(33).setCellValue(row.getRowNum()+ "," +cellIndex + "单元格输入不合法!");
+            //throw new RuntimeException(row.getRowNum()+ "," +cellIndex + "单元格输入不合法!");
+        }else {
+            sb.append(attrName).append("=\"").append(row.getCell(cellIndex).getStringCellValue()).append("\" ");
         }
         return sb;
     }
